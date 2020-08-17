d793cb6dde14f2d5dafd5e1bf2404fa4f35ac05e
Zatsune No Mokou <contact@zatsunenomokou.eu>
2020-08-17 17:25:28 +0200

    rework/refactor

--- webextension/index.js
+++ webextension/index.js
@@ -1,15 +1,28 @@
 'use strict';
 
+let env = null;
+/**
+ *
+ * @return {Promise<string>}
+ */
+async function getEnv() {
+	if (env === null) {
+		const { default:_env } = await import('./env.js');
+		env = _env;
+	}
+
+	return env;
+}
+
 let _ = chrome.i18n.getMessage;
 
-class notifAction{
-	constructor(type, data){
-		this.type = type;
-		this.data = data;
-	}
-}
-let chromeAPI_button_availability = true;
-function doNotif(title, message){
+/**
+ *
+ * @param {string} title
+ * @param {string} message
+ * @return {Promise<void>}
+ */
+function doNotif(title, message) {
 	let options = {
 		type: "basic",
 		title: title,
@@ -18,64 +31,76 @@ function doNotif(title, message){
 		iconUrl: "/icon.png",
 		isClickable: true
 	};
-	
-	let close = {title: _("Close"), iconUrl: "/data/ic_close_black_24px.svg"};
-	
-	if(chromeAPI_button_availability === true){
-		options.buttons = [close];
-	}
-	
-	const notification_id = JSON.stringify(new notifAction("none", {timestamp: Date.now()}));
-	
-	new Promise((resolve, reject) => {
-		chrome.notifications.create(notification_id, options, function(notificationId){
+
+	return new Promise((resolve, reject) => {
+		chrome.notifications.create(options, function() {
 			if(typeof chrome.runtime.lastError === "object" && chrome.runtime.lastError !== null && typeof chrome.runtime.lastError.message === "string" && chrome.runtime.lastError.message.length > 0){
 				reject(chrome.runtime.lastError);
+			} else {
+				resolve();
 			}
 		});
-	}).catch((error)=> {
-		if(typeof error === "object" && typeof error.message === "string" && error.message.length > 0){
-			console.group();
-			console.warn(error.message);
-			console.dir(error);
-			console.groupEnd();
-
-			if(error.message === "Adding buttons to notifications is not supported." || error.message.indexOf("\"buttons\"") !== -1){
-				chromeAPI_button_availability = false;
-				console.log("Buttons not supported, retrying notification without them.");
-				doNotif(title, message);
-			}
-		}
 	})
+		.catch(error => {
+			if (typeof error === "object" && typeof error.message === "string" && error.message.length > 0) {
+				console.error(error);
+			}
+		})
+	;
 }
-function onNotificationClick(notificationId){
+chrome.notifications.onClicked.addListener(function (notificationId) {
 	console.info(`${notificationId} (onClicked)`);
 	chrome.notifications.clear(notificationId);
-}
-chrome.notifications.onButtonClicked.addListener(onNotificationClick);
-chrome.notifications.onClicked.addListener(onNotificationClick);
+});
 
-function isRightURL(URL){
-	let test_url = /(?:http|https):\/\/.+/;
-	return (typeof URL === "string" && test_url.test(URL));
+
+
+
+
+function copyToClipboard(string) {
+	if (document.querySelector('#copy_form') !== null) {
+		let node = document.querySelector('#copy_form');
+		node.parentNode.removeChild(node);
+	}
+
+	let copy_form = document.createElement('textarea');
+	copy_form.id = 'copy_form';
+	copy_form.textContent = string;
+	//copy_form.class = "hide";
+	copy_form.setAttribute('style', 'height: 0 !important; width: 0 !important; border: none !important; z-index: -9999999;');
+	document.querySelector('body').appendChild(copy_form);
+
+	//copy_form.focus();
+	copy_form.select();
+
+	let clipboard_success = document.execCommand('Copy');
+	copy_form.parentNode.removeChild(copy_form);
+
+	return clipboard_success;
 }
+
+
+
+
 
 chrome.contextMenus.removeAll();
 chrome.contextMenus.create({
-	"title":_("Copy_link_text"),
-	"contexts": ["link"],
-	"targetUrlPatterns": ["http://*/*", "https://*/*"],
-	"onclick": function(info, tab){
-		chrome.tabs.sendMessage(tab.id, {
-			id: "copyLinkText",
-			data: ""
-		}, function(responseData){
-			const copyLinkText_success = responseData.copyLinkText_success,
-				string = responseData.string;
-			if(!copyLinkText_success || chrome.runtime.getManifest().name.indexOf("Dev")!==-1){
-				doNotif(_("copy_result"), (copyLinkText_success)? _("Copied_link_text") : _("Error_when_copying_to_clipboad"));
-			}
-			console.warn(`Copy to clipboad ${(copyLinkText_success)? "success" : "error"} (${string})`);
-		});
-	}
+	id: 'link_CopyTextLink',
+	title:_("Copy_link_text"),
+	contexts: ["link"],
+	targetUrlPatterns: ["http://*/*", "https://*/*"]
 });
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+	chrome.tabs.sendMessage(tab.id, {
+		id: "copyLinkText",
+		data: ""
+	}, async function (responseData) {
+		let clipboardResult = (typeof responseData === 'object' && responseData !== null) && copyToClipboard(responseData.string);
+
+		if (!clipboardResult || (await getEnv()) !== 'prod') {
+			doNotif(_("copy_result"), (clipboardResult) ? _("Copied_link_text") : _("Error_when_copying_to_clipboad"));
+		}
+
+		console[(clipboardResult) ? "debug" : "warn"](`Copy to clipboad ${(clipboardResult) ? "success" : "error"} (${responseData?.string})`);
+	});
+})
